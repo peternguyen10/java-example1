350d965bcd595e18fb0fb81cd73a642e62b9ae98
peternguyen10 <peter.nguyen1@gmail.com>
2020-10-16 18:23:51 +0000

    f

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,11 +3,13 @@ public class Fibonacci {
     public static void main(String[] args) {
 
         int n = 10, t1 = 0, t2 = 1;
+        System.out.println("running Fibonacci number");
         System.out.print("First " + n + " terms: ");
 
         for (int i = 1; i <= n; ++i)
         {
             System.out.print(t1 + " + ");
+                    
 
             int sum = t1 + t2;
             t1 = t2;
