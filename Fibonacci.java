6cfdf49b369f36be4c7f8397bb254852b0d267b9
peternguyen10 <peter.nguyen1@gmail.com>
2020-10-30 11:41:51 -0400

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,14 +3,14 @@ public class Fibonacci {
     public static void main(String[] args) {
 
         int n = 10, t1 = 0, t2 = 1;
-        System.out.println("running Fibonacci number");
+        System.out.println("running Fibonacci number demo in the container");
         System.out.println("Running a demo setting up remote server connecting thru ssh and spin up a container dev en.");
         System.out.print("First " + n + " terms: ");
 
         for (int i = 1; i <= n; ++i)
         {
             System.out.print(t1 + " + ");
-                                
+                                            
 
             int sum = t1 + t2;
             t1 = t2;
