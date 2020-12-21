a80cd0783e61def59a722befa944460032632069
peternguyen10 <peter.nguyen1@gmail.com>
2020-12-21 15:22:23 +0000

    remove version

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,14 +3,15 @@ public class Fibonacci {
     public static void main(String[] args) {
 
         int n = 10, t1 = 0, t2 = 1;
-        System.out.println("running Fibonacci number demo in container running on remote host openjdk14");
+        System.out.println("running Fibonacci number demo in container running on remote host");
        
         System.out.print("First " + n + " terms:.");
 
         for (int i = 1; i <= n; ++i)
         {
             System.out.println(t1 + " + ");
-                        
+                    
+                                            
             int sum = t1 + t2;
             t1 = t2;
             t2 = sum;
